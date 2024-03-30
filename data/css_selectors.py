16297c8342887063c9a3df905a9708bcdb93f279
sndmndss <yanfedorov120505@gmail.com>
2024-03-30 14:10:23 -0700

    Fix for other browser

--- data/css_selectors.py
+++ data/css_selectors.py
@@ -1,26 +1,26 @@
 RAINBOW_INPUT_CSS_SELECTOR = (
-    "#main > div > div._28iyjl0._28iyjl5w._28iyjlj6.lt-dc.dt-dc > div > div:nth-child(1) > div > div > div >"
-    " div._28iyjl0._28iyjlf8._28iyjlgf > div._28iyjl0._28iyjl6o._28iyjl6s._28iyjl51._28iyjl7k > "
-    "div._28iyjl0._28iyjlim > div > div > input"
+    "#main > div > div._28iyjl0._28iyjl5w._28iyjlj7.lt-dc.dt-dc > div > div:nth-child(1) > div > div > div > "
+    "div._28iyjl0._28iyjlf9._28iyjlgg > div._28iyjl0._28iyjl6o._28iyjl6s._28iyjl51._28iyjl7l > "
+    "div._28iyjl0._28iyjlin > div > div > input"
 )
 RAINBOW_BUTTON_CSS_SELECTOR = (
-    "#main > div > div._28iyjl0._28iyjl5w._28iyjlj6.lt-dc.dt-dc > div > div:nth-child(1) > div > "
-    "div > div > div._28iyjl0._28iyjlf8._28iyjlgf > div._28iyjl0._28iyjlim._28iyjlhf._28iyjle1 > div > button"
+    "#main > div > div._28iyjl0._28iyjl5w._28iyjlj7.lt-dc.dt-dc > div > div:nth-child(1) > div > div > div > "
+    "div._28iyjl0._28iyjlf9._28iyjlgg > div._28iyjl0._28iyjlin._28iyjlhg._28iyjle2 > div > button"
 )
 RAINBOW_PASSWORD_1 = (
-    "#main > div > div._28iyjl0._28iyjl5w._28iyjlj6.lt-dc.dt-dc > div > div:nth-child(1) > "
-    "div > div > div > div._28iyjl0._28iyjlf8._28iyjlgf._28iyjl6o._28iyjl6s._28iyjl51 > div > div:nth-child(1) "
-    "> div._28iyjl0._28iyjlho._28iyjlim > div > div:nth-child(1) > div > div:nth-child(2) > div "
-    "> div._28iyjl88._28iyjlim > input"
+    "#main > div > div._28iyjl0._28iyjl5w._28iyjlj7.lt-dc.dt-dc > div > div:nth-child(1) > div > div > "
+    "div > div._28iyjl0._28iyjlf9._28iyjlgg._28iyjl6o._28iyjl6s._28iyjl51 > div >"
+    " div:nth-child(1) > div._28iyjl0._28iyjlhp._28iyjlin > div > div:nth-child(1) > div > "
+    "div:nth-child(2) > div > div._28iyjl89._28iyjlin > input"
 )
 RAINBOW_PASSWORD_2 = (
-    "#main > div > div._28iyjl0._28iyjl5w._28iyjlj6.lt-dc.dt-dc > div > div:nth-child(1) > div > "
-    "div > div > div._28iyjl0._28iyjlf8._28iyjlgf._28iyjl6o._28iyjl6s._28iyjl51 > div > div:nth-child(1) "
-    "> div._28iyjl0._28iyjlho._28iyjlim > div > div:nth-child(2) > "
-    "div > div:nth-child(2) > div > div:nth-child(1) > div > div._28iyjl88._28iyjlim > input"
+    "#main > div > div._28iyjl0._28iyjl5w._28iyjlj7.lt-dc.dt-dc > div > div:nth-child(1) > div > div > div > "
+    "div._28iyjl0._28iyjlf9._28iyjlgg._28iyjl6o._28iyjl6s._28iyjl51 > div > div:nth-child(1) > "
+    "div._28iyjl0._28iyjlhp._28iyjlin > div > div:nth-child(2) > div > div:nth-child(2) > div > div:nth-child(1) "
+    "> div > div._28iyjl89._28iyjlin > input"
 )
 RAINBOW_BUTTON_2_CSS_SELECTOR = (
-    "#main > div > div._28iyjl0._28iyjl5w._28iyjlj6.lt-dc.dt-dc > div > div:nth-child(1) > div > div > div > "
-    "div._28iyjl0._28iyjlf8._28iyjlgf._28iyjl6o._28iyjl6s._28iyjl51 > div > div:nth-child(2) > div > div > "
-    "div > div > button"
+    "#main > div > div._28iyjl0._28iyjl5w._28iyjlj7.lt-dc.dt-dc > div > div:nth-child(1) > div > div > div > "
+    "div._28iyjl0._28iyjlf9._28iyjlgg._28iyjl6o._28iyjl6s._28iyjl51 > div > div:nth-child(2) > div > div"
+    " > div > div > button"
 )
