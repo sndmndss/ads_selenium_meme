2403ad213e638ab67845f1e6632288b7b9790e76
sndmndss <yanfedorov120505@gmail.com>
2024-04-02 15:00:16 +0200

    Hot fix

--- modules/rabby.py
+++ modules/rabby.py
@@ -13,16 +13,20 @@ def rabby_login(driver, private_key, password=8901324567):
     rabby_button1 = wait.until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "#root > div > div.rabby-container.pb-\[12px\] > div:nth-child(2) > div:nth-child(2)")))
     rabby_button1.click()
+    sleep(1)
     input_confirm = driver.find_element(By.ID, 'password')
     input_confirm.send_keys(password)
+    sleep(1)
     input_element = driver.find_element(By.ID, 'confirmPassword')
     input_element.send_keys(password)
+    sleep(1)
     rabby_button2 = wait.until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "#root > div > div > div > form > div.p-32.pt-0 > button")))
     rabby_button2.click()
-    sleep(2)
+    sleep(1)
     input_key = driver.find_element(By.ID, 'key')
     input_key.send_keys(private_key)
+    sleep(1)
     rabby_button3 = wait.until(
         EC.element_to_be_clickable((By.CSS_SELECTOR,
                                     "#root > div > form > div.bottom-0.left-0.w-full.flex.lg\:bottom-\[-24px\].z-10.footer.fixed > div > button")))
