b575bf0e7ec52eb9f79c1fe7194bcaa1b2e03f15
sndmndss <yanfedorov120505@gmail.com>
2024-04-02 15:09:19 +0200

    better EC for waiting

--- modules/rabby.py
+++ modules/rabby.py
@@ -11,16 +11,16 @@ def rabby_login(driver, private_key, password=8901324567):
     driver.get(RABBY_URL)
     print("PRESS F5 IN BROWSER!")
     rabby_button1 = wait.until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#root > div > div.rabby-container.pb-\[12px\] > div:nth-child(2) > div:nth-child(2)")))
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "#root > div > div.rabby-container.pb-\[12px\] > div:nth-child(2) > div:nth-child(2)")))
     rabby_button1.click()
-    input_confirm = wait.until(EC.presence_of_element_located((By.ID, 'password')))
+    input_confirm = wait.until(EC.element_to_be_clickable((By.ID, 'password')))
     input_confirm.send_keys(password)
-    input_element = wait.until(EC.presence_of_element_located((By.ID, 'confirmPassword')))
+    input_element = wait.until(EC.element_to_be_clickable((By.ID, 'confirmPassword')))
     input_element.send_keys(password)
     rabby_button2 = wait.until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "#root > div > div > div > form > div.p-32.pt-0 > button")))
     rabby_button2.click()
-    input_key = wait.until(EC.presence_of_element_located((By.ID, 'key')))
+    input_key = wait.until(EC.element_to_be_clickable((By.ID, 'key')))
     input_key.send_keys(private_key)
     rabby_button3 = wait.until(
         EC.element_to_be_clickable((By.CSS_SELECTOR,
